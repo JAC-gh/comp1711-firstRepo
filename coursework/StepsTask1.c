c4bcd3645dd6d10718f332a29a08bcd397faf6ac
Jamie Alexander Clarke <146664973+JAC-gh@users.noreply.github.com>
2023-11-12 01:34:41 +0000

    coursework1_task2_attempt1

---
+++ coursework/StepsTask1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    FITNESS_DATA data[150];
+    FILE* csvFile = fopen("FitnessData_2023.csv", "r");
+
+    char fileOut[50], dateOfSteps[11], timeOfSteps[6], noOfSteps[7];
+    int i;
+    while (fgets(fileOut, 50, csvFile) != NULL){
+        tokeniseRecord(fileOut, ",", dateOfSteps, timeOfSteps, noOfSteps);
+        strcpy(data[i].date, dateOfSteps);
+        strcpy(data[i].time, timeOfSteps);
+        data[i].steps = atoi(noOfSteps);
+        i++;
+    }
+
+    printf("Number of records in file: %d\n", i);
+    for (i = 0; i < 3; i++){
+    printf("%s/%s/%d\n", data[i].date, data[i].time, data[i].steps);
+    }
+
+    fclose(csvFile);
+    return 0;
+}
--- coursework/task2.c
+++ coursework/task2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "FitnessDataStruct.h"
+#include <math.h>
 
 // Struct moved to header file
 
@@ -39,14 +40,18 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 
 
-
-
 // Complete the main function
 int main() {
+    // initialises array of structs
+    FITNESS_DATA data[150];
+    // initialises required variables
+    char userChoice;
+    int i=0, totalSteps=0, indexOfLowest=0, indexOfMax=0, meanSteps;
+    int max500, current500, max500Start, current500Start, max500End;
+    char filename[20], fileOut[50], dateOfSteps[11], timeOfSteps[6], noOfSteps[7];
+    //loops forever until user exits through Q or code forced to quit
     while (1){
-        char userChoice;
-        int i, meanSteps;
-        char filename[20], fileOut[20], perStart[20], perEnd[20];
+        //displays menu options
         printf("Menu Options:\nA: Specify the filename to be imported\
                 \nB: Display the total number of records in the file\
                 \nC: Find the date and time of the timeslot with the fewest steps\
@@ -54,31 +59,98 @@ int main() {
                 \nE: Find the mean step count of all records in the file\
                 \nF: Find the longest continuous period where the step count is above 500 steps\
                 \nQ: Quit\nEnter Choice:");
+        //takes in user input
         scanf("%c", &userChoice);
+        //stops scanf from using leftover return character from entry in future loops
+        while (userChoice == '\n'){
+            scanf("%c", &userChoice);
+        }
+        //switch statement based on user input
         switch (userChoice){
-            case 'a':
+            case 'A':
+                //tries to open file using name input by user
                 printf("Input filename: ");
                 scanf("%s", filename);
-                continue;
-            case 'b':
-                printf("Total Records: %d", i);
-                continue;
-            case 'c':
-                printf("Fewest Steps: %s", fileOut);
-                continue;
-            case 'd':
-                printf("Largest Steps: %s", fileOut);
-                continue;
-            case 'e':
-                printf("Mean Step Count: %d", meanSteps);
-                continue;
-            case 'f':
-                printf("Longest Period Start: %s\
-                        Longest Period End: %s", perStart, perEnd);
-                continue;
-            case 'q':
+                FILE *fileCheck = fopen(filename, "r");
+                if (fileCheck == NULL){
+                    //returns error if file not found
+                    printf("Error: Could not find or open file.\n");
+                    return 1;
+                }
+                //loop to read content out of the file
+                while (fgets(fileOut, 50, fileCheck) != NULL){
+                    tokeniseRecord(fileOut, ",", dateOfSteps, timeOfSteps, noOfSteps);
+                    strcpy(data[i].date, dateOfSteps);
+                    strcpy(data[i].time, timeOfSteps);
+                    data[i].steps = atoi(noOfSteps);
+                    if (atoi(noOfSteps) < data[indexOfLowest].steps){
+                        //keeps track of the record with lowest step count via index in data
+                        indexOfLowest = i;
+                    }
+                    if (atoi(noOfSteps) > data[indexOfMax].steps){
+                        //keeps track of the record with highest step count via index in data
+                        indexOfMax = i;
+                    }
+                    //updates total step count
+                    totalSteps += atoi(noOfSteps);
+                    if (atoi(noOfSteps) > 500){
+                        if (current500 == 0){
+                            //updates start of current 500+ streak
+                            current500Start = i;
+                        }
+                        //updates length of current 500+ streak
+                        current500 += 1;
+                    }
+                    else if (atoi(noOfSteps) <= 500){
+                        if (current500 != 0){
+                            //checks if completed 500+ streak is longer than previous best
+                            if (current500 > max500){
+                                //if it is, updates stats for longest streak
+                                max500 = current500;
+                                max500Start = current500Start;
+                            }
+                        }
+                        //resets length of current 500+ streak
+                        current500 = 0;
+                    }
+                    //updates record count
+                    i++;
+                }
+                //calculates mean steps
+                meanSteps = round(totalSteps/i);
+                //holds index of the last record in the longest 500+ streak
+                max500End = max500Start + max500 - 1;
+                break;
+            case 'B':
+                //prints total number of records in the file
+                printf("Total Records: %d\n", i);
+                break;
+            case 'C':
+                //prints date and time of record with the lowest steps in the file
+                printf("Fewest Steps: %s %s\n", data[indexOfLowest].date, data[indexOfLowest].time);
+                break;
+            case 'D':
+                //prints date and time of record with the highest steps in the file
+                printf("Largest Steps: %s %s\n", data[indexOfMax].date, data[indexOfMax].time);
+                break;
+            case 'E':
+                //prints mean steps of entire file
+                printf("Mean Step Count: %d\n", meanSteps);
+                break;
+            case 'F':
+                //prints date and time of start and end of the 500+ streak
+                printf("Longest Period Start: %s %s\n", data[max500Start].date, data[max500Start].time);
+                printf("Longest Period End: %s %s\n", data[max500End].date, data[max500End].time);
+                break;
+            case 'Q':
+                //exits program and returns 0
                 return 0;
+            default:
+                //prints 'error' message if user input not between A-F or Q and reoutputs menu
+                printf("Invalid Option...Please Try Again...\n\n");
+                break;
         }
     }
 }
+
 
